9f9d81ad0693f4357613854e4164c3142e163b70
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2023-10-12 13:03:00 +0500

    querysearch

--- index.js
+++ index.js
@@ -43,4 +43,34 @@ app.get('/products/:productId/reviews', async (req, res) => {
     }
 });
 
+// GET Product Offers
+app.get('/products/:productId/offers', async (req, res) => {
+    const {
+        productId
+    } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+});
+
+// GET Search Result
+app.get('/search/:searchQuery', async (req, res) => {
+    const {
+        searchQuery
+    } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/s?k=/${searchQuery}`);
+
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+});
+
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
